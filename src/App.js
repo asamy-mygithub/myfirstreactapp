0bcf05160beb86f91126329801fb7ab72d49313a
asamy-mygithub <kjajjaaws@gmail.com>
2020-12-20 15:42:27 -0500

    app.js disp text 2

--- src/App.js
+++ src/App.js
@@ -9,14 +9,19 @@ function App() {
         <p>
           !!! Hello from Alfred !!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table width="100%" border="2px" height="500px">
+          <tbody>
+            <tr height="20%">
+              <td><h1>This is customer HEADER Part</h1></td>
+            </tr>
+            <tr height="70%">
+            <td><h1>This is customer INFO Part</h1></td>
+            </tr>
+            <tr height="10%">
+            <td><h1>This is customer FOOTER Part</h1></td>
+            </tr>
+          </tbody>
+        </table>
       </header>
     </div>
   );
