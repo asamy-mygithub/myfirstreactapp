104a9a07952f6ecd1e3831bec7000fa0642bb006
asamy-mygithub <kjajjaaws@gmail.com>
2020-12-22 10:53:30 -0500

    changed js file

--- src/App.js
+++ src/App.js
@@ -4,21 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to React JS
-        </p>
+      <header className="App-header">        
         <table width="100%" border="2px" height="500px">
           <tbody>
-            <tr height="20%">
-              <td><h1>This is customer HEADER Part</h1></td>
-            </tr>
-            <tr height="70%">
-            <td><h1>This is customer INFO Part</h1></td>
-            </tr>
-            <tr height="10%">
-            <td><h1>This is customer FOOTER Part</h1></td>
+            <tr>
+              <td>
+                <h1>!!! This site is under construction !!!</h1>
+              </td>
             </tr>
           </tbody>
         </table>
